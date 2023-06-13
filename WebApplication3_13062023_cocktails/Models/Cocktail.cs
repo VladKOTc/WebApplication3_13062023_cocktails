b75d91086d7ce2930894dc82f768b9c655bbe73d
VladKOTc <100993516+VladKOTc@users.noreply.github.com>
2023-06-13 16:49:43 +0300

    Добавьте файлы проекта.

---
+++ WebApplication3_13062023_cocktails/Models/Cocktail.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace WebApplication3_13062023_cocktails.Models
+{
+    public class Cocktail
+    { 
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ WebApplication3_13062023_cocktails/Pages/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication3_13062023_cocktails.Pages
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
